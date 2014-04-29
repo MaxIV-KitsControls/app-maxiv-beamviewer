6772cd364db1eb6bc96e89ceba188c34c8f847e2
Johan Forsberg <johan.forsberg@maxlab.lu.se>
2014-04-29 13:59:19 +0200

    add desktop file

--- setup.py
+++ setup.py
@@ -2,16 +2,18 @@
 
 from setuptools import setup
 
-setup(name = "taurusgui-beamviewer",
-      version = "0.7.0",
-      description = "TaurusGUI for viewing YAG screens",
-      author = "Johan Forsberg",
-      author_email = "johan.forsberg@maxlab.lu.se",
-      license = "GPLv3",
-      url = "http://www.maxlab.lu.se",
-      package_dir = {'':'src',},
-      packages = ['tgconf_beamviewer', 'tgconf_beamviewer.panels'],
-      include_package_data=True,
-      package_data={'tgconf_beamviewer': ['default.ini', 'images/MAXlogo_liten.jpg']},
-      scripts = ['scripts/ctbeamviewer']
+setup(
+    name="taurusgui-beamviewer",
+    version="0.7.0",
+    description="GUI for viewing YAG screens",
+    author="Johan Forsberg",
+    author_email="johan.forsberg@maxlab.lu.se",
+    license="GPLv3",
+    url="http://www.maxlab.lu.se",
+    package_dir={'': 'src'},
+    packages=['tgconf_beamviewer', 'tgconf_beamviewer.panels'],
+    include_package_data=True,
+    package_data={'tgconf_beamviewer': ['default.ini', 'images/MAXlogo_liten.jpg']},
+    data_files=[('/usr/share/applications', ['maxiv-beamviewer.desktop'])],
+    scripts=['scripts/ctbeamviewer']
 )
