6eee5354a73601cd06b719043f5e0042d1bf0d06
Vincent Michel <vincent.michel@maxlab.lu.se>
2014-08-20 16:31:22 +0200

    RT5457	Ordering of devices in beamviewer camera selector - Ordering depends on the __s_cen property

--- src/tgconf_beamviewer/panels/cameraselector.py
+++ src/tgconf_beamviewer/panels/cameraselector.py
@@ -1,21 +1,33 @@
 
 from taurus.qt import QtCore, QtGui
-from taurus import Database
+from PyTango import Database
 
 
 class CameraSelector(QtGui.QComboBox):
     domain = 'lima'
     family = 'limaccd'
+    pos = '__s_cen'
 
     def __init__(self, parent=None):
         QtGui.QComboBox.__init__(self, parent)
         self._addItems()
     
     def _addItems(self):
+        # Access the database
         db = Database()
-        for member in db.getDeviceMemberNames(self.domain, self.family):
-            item = '%s/%s/%s' % (self.domain, self.family, member)
-            self.addItem(item)
+        device_dct = {}
+        wildcard = "/".join((self.domain, self.family, '*'))
+        # Build device to postion dictionary
+        for member in db.get_device_member(wildcard):
+            device = "/".join((self.domain, self.family, member))
+            prop = db.get_device_property(device, self.pos)[self.pos]
+            try:
+                device_dct[device] = float(prop[0])
+            except (ValueError, IndexError):
+                device_dct[device] = float('inf')
+        # Add item in sorted order
+        for device in sorted(device_dct, key=device_dct.get):
+            self.addItem(device)
 
 
 def main():
