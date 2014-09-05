2822d136703a1922e8c0597c3838c46d3d7f3527
Johan Forsberg <johan.forsberg@maxlab.lu.se>
2014-09-05 10:04:02 +0200

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="taurusgui-beamviewer",
-    version="0.9.1",
+    version="0.10.0",
     description="GUI for viewing YAG screens",
     author="Johan Forsberg",
     author_email="johan.forsberg@maxlab.lu.se",
