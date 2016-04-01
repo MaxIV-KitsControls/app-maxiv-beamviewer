83b8646a836859b10be51910a97a2fe6fdcd7b06
Vasileios Martos <vasileios.martos@maxiv.lu.se>
2016-04-01 16:50:58 +0200

    Automatic Equalisation of Desktop menu in control-room Workstations #9558

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="taurusgui-beamviewer",
-    version="0.12.1",
+    version="0.12.2",
     description="GUI for viewing YAG screens",
     author="Johan Forsberg",
     author_email="johan.forsberg@maxlab.lu.se",
